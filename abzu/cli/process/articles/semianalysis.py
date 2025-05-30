91e1cec30546c495a6aa2c5efeefa41de4f5e13f
Russell Jurney <russell.jurney@gmail.com>
2025-05-30 18:08:34 +0100

    Externalized path on CLI for abzu.process.articles.semianalysis

--- abzu/cli/process/articles/semianalysis.py
+++ abzu/cli/process/articles/semianalysis.py
@@ -2,21 +2,23 @@
 
 import click
 
+from abzu.config import config
+
 
 @click.command(context_settings={"show_default": True})
 @click.option(
     "-i",
     "--input",
     "input_file",
-    default="data/semianalysis.jsonl",
-    help="Input JSONL file path (default: data/semianalysis.jsonl)",
+    default=config.get("process.articles.semianalysis.input"),
+    help="Input JSONL file path",
 )
 @click.option(
     "-o",
     "--output",
     "output_file",
-    default="data/processed_semianalysis.jsonl",
-    help="Output JSONL file path (default: data/processed_semianalysis.jsonl)",
+    default=config.get("process.articles.semianalysis.output"),
+    help="Output JSONL file path",
 )
 @click.option(
     "-b",
