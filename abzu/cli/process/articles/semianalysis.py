1649919ebf258c3648e0bde4751b1c86cfc709bc
Russell Jurney <russell.jurney@gmail.com>
2025-05-30 18:58:54 +0100

    Added path types to CLI for 'abzu process articles semianalysis'

--- abzu/cli/process/articles/semianalysis.py
+++ abzu/cli/process/articles/semianalysis.py
@@ -11,6 +11,7 @@ from abzu.config import config
     "--input",
     "input_file",
     default=config.get("process.articles.semianalysis.input"),
+    type=click.Path(exists=True, dir_okay=False, file_okay=True, path_type=str),
     help="Input JSONL file path",
 )
 @click.option(
@@ -18,6 +19,7 @@ from abzu.config import config
     "--output",
     "output_file",
     default=config.get("process.articles.semianalysis.output"),
+    type=click.Path(exists=True, dir_okay=False, file_okay=True, path_type=str),
     help="Output JSONL file path",
 )
 @click.option(
