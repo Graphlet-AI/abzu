04f86f8220e5ba18ca889a643d65ea66a67decca
Russell Jurney <russell.jurney@gmail.com>
2025-05-30 19:19:40 +0100

    Added click.Path type for 'abzu dump company-ticker-resolution'

--- abzu/cli/dump/company_ticker_resolution.py
+++ abzu/cli/dump/company_ticker_resolution.py
@@ -11,6 +11,7 @@ from abzu.config import config
     "--file",
     "input_file",
     default=config.get("dump.companies.input"),
+    type=click.Path(exists=True, dir_okay=True, file_okay=True, path_type=str),
     help="Path to companies.parquet file",
 )
 def company_ticker_resolution(input_file: str) -> int:
